52c1715619f74ae6e82aea7be4c7c07a6f1d16b5
swagata-codearts <swagata.codearts@gmail.com>
2022-11-08 11:49:50 +0530

    Add bootstrapV5 framework

---
+++ js/custom.js
@@ -0,0 +1,17 @@
+// JavaScript Document
+jQuery('#custom-owl').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+});
