1365bbc7b349e1face77c88e87b630cbf9176c67
swagata-codearts <swagata.codearts@gmail.com>
2022-11-11 19:11:14 +0530

    client-photos js

--- js/custom.js
+++ js/custom.js
@@ -16,7 +16,25 @@ jQuery('#custom-owl').owlCarousel({
     }
 });
 
-jQuery(document).ready(function() {
-    jQuery(".client-gallery .custom-imglist [class*='col-']").slice(0, 3).show();
-})
 
+jQuery(document).ready(function () {
+    jQuery('.custom-imglist .col-lg-4:lt(6)').show();
+    jQuery('.show-less').hide();
+    var items =  9;
+    var shown =  3;
+    jQuery('.load-more').click(function () {
+        jQuery('.show-less').show();
+        shown = jQuery('.custom-imglist .col-lg-4:visible').length+3;
+        if(shown< items) {
+          jQuery('.custom-imglist .col-lg-4:lt('+shown+')').show(300);
+        } else {
+          jQuery('.custom-imglist .col-lg-4:lt('+items+')').show(300);
+          jQuery('.load-more').hide();
+        }
+    });
+    jQuery('.show-less').click(function () {
+        jQuery('.custom-imglist .col-lg-4').not(':lt(6)').hide(300);
+        jQuery('.load-more').show();
+        jQuery('.show-less').hide();
+    });
+});
