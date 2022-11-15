775008838af590ee32dad6dca9c3b163a5436e05
swagata-codearts <swagata.codearts@gmail.com>
2022-11-15 14:57:53 +0530

    About Us Page Checked

--- js/custom.js
+++ js/custom.js
@@ -1,16 +1,20 @@
 // JavaScript Document
-jQuery('#custom-owl').owlCarousel({
+jQuery('#contact-location-carousel').owlCarousel({
     loop:true,
     margin:10,
     nav:true,
+    navText : ['<i class="fas fa-chevron-left"></i>','<i class="fas fa-chevron-right"></i>'],
     responsive:{
         0:{
+            items:1
+        },
+        768:{
             items:2
         },
-        600:{
-            items:2
+        992:{
+            items:3
         },
-        1000:{
+        1500:{
             items:4
         }
     }
